fbb07070f5793473697cffd538d114fb5cbc02c6
prabhakar9885 <prabhakar.9885@gmail.com>
2015-11-17 23:15:09 +0530

    Fixed a bug. Displays posts in cummulative order.

--- controllers/user.py
+++ controllers/user.py
@@ -10,22 +10,26 @@ def home():
 	db.tbl_posts.data_content.readable = False
 	db.tbl_posts.data_content.writable = False
 	form = SQLFORM(db.tbl_posts);
-	posts = db(db.tbl_posts.created_by==loggedin_User_id or 
-						db.tbl_posts.shared_with==loggedin_User_id).select();
+	posts = db( (db.tbl_posts.created_by==loggedin_User_id) | 
+						(db.tbl_posts.shared_with==loggedin_User_id) ).select( orderby=~db.tbl_posts.created_on );
 	i=0;
 	post_cleaned=[]
 	for post in posts:
 		post_cleaned.append(post);
 		creater = db(post_cleaned[i].created_by ==db.auth_user.id).select()[0];
 		post_cleaned[i].created_by = "%s %s" %(creater.first_name, creater.last_name);
-		shared_with = db(post_cleaned[i].shared_with ==db.auth_user.id).select()[0];
-		post_cleaned[i].shared_with = "%s %s" %(shared_with.first_name, shared_with.last_name);
+		try:
+			shared_with = db(post_cleaned[i].shared_with ==db.auth_user.id).select()[0];
+			post_cleaned[i].shared_with = "%s %s" %(shared_with.first_name, shared_with.last_name);
+		except IndexError: # The post is shared with none.
+			post_cleaned[i].shared_with = "";
 		i = i+1;
 	# options = form.elements('option');
 	# for i in options:
 	# 	if i.attributes["_value"]==str(loggedin_User_id):
 	# 		options.remove( i );
 	# form.element('option',replace=options);
+	
 	return locals();
 
 def post_status():
