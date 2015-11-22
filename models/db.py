bdfe87a20fecc8ec36d1f09edcb6e79790dd7710
prabhakar9885 <prabhakar.9885@gmail.com>
2015-11-22 16:12:46 +0530

    Mail module is integrated.

--- models/db.py
+++ models/db.py
@@ -59,9 +59,16 @@ plugins = PluginManager()
 
 ## configure email
 mail = auth.settings.mailer
-mail.settings.server = 'logging' if request.is_local else myconf.take('smtp.server')
-mail.settings.sender = myconf.take('smtp.sender')
-mail.settings.login = myconf.take('smtp.login')
+# mail.settings.server = 'logging' if request.is_local else myconf.take('smtp.server')
+# mail.settings.sender = myconf.take('smtp.sender')
+# mail.settings.login = myconf.take('smtp.login')
+# 
+mail.settings.server = 'smtp.gmail.com:587'
+mail.settings.sender = 'prabhakar.9885@gmail.com'
+mail.settings.login = 'prabhakar.9885@gmail.com:16-digit-Google-App-specific-password'
+
+# mail.send(to=[],subjet='',message='')
+
 
 ## configure auth policy
 auth.settings.registration_requires_verification = False
